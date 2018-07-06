81753df04427f54b150b1c62e88f20553e6ce533
bentc <bentc.learningtest@outlook.com>
2018-07-05 23:07:01 -0500

    Performing first commit.

---
+++ First_VS_Git_Project/First_VS_Git_Project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_VS_Git_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
